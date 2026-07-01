e9cd165726d88bc14f9b3da7e5673155e0ecce30
SabievM <msabieff@gmail.com>
2026-07-02 01:57:51 +0300

    Корректировка шаблонов рассылки

--- newsletter/services.py
+++ newsletter/services.py
@@ -4,6 +4,11 @@ from django.template.loader import render_to_string
 from django.core.mail import EmailMultiAlternatives
 from .models import Subscriber
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+ENVIRONMENT = os.environ.get("ENVIRONMENT", "development")
 
 def send_products_email(products, mail_type):
 
@@ -16,13 +21,26 @@ def send_products_email(products, mail_type):
         "sale": "emails/sale.html",
         "collection": "emails/collection.html",
     }
+    if ENVIRONMENT == "development":
+        backend_base_url = "http://localhost:8000"
+        frontend_base_url = "http://localhost:3000"
+    else:
+        backend_base_url = os.environ.get("BACKEND_BASE_URL")
+        frontend_base_url = os.environ.get("FRONTEND_BASE_URL")
 
     template = template_map.get(mail_type)
+
+    if products[0].get("type") == "showerassembly":
+        catalog_products = "plumbing-fixtures"
+    else:
+        catalog_products = products[0].get("type")
+    
+    catalog_url = f"{frontend_base_url}/products/{catalog_products}"
 
     for subscriber in subscribers:
 
         unsubscribe_url = (
-            f"http://localhost:8000/api/newsletter/unsubscribe/{subscriber.unsubscribe_token}/"
+            f"{backend_base_url}/api/newsletter/unsubscribe/{subscriber.unsubscribe_token}/"
         )
 
         html = render_to_string(
@@ -30,6 +48,8 @@ def send_products_email(products, mail_type):
             {
                 "products": products,
                 "unsubscribe_url": unsubscribe_url,
+                "catalog_url": catalog_url,
+                "catalog_products": catalog_products,
             }
         )
 
